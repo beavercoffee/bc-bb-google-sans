805f6ee9bf455de5f55657cb2243a14f5fd81e67
beavercoffee <beavercoffee@vidsoe.com>
2021-07-11 16:58:24 -0600

    BC

--- classes/class-bc-bb-google-sans.php
+++ classes/class-bc-bb-google-sans.php
@@ -17,14 +17,17 @@ if(!class_exists('BC_BB_Google_Sans')){
     	//
     	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-    	public static function get_instance($file = ''){
-            if(null === self::$instance){
-                if(@is_file($file)){
-                    self::$instance = new self($file);
-                } else {
-                    wp_die(__('File doesn&#8217;t exist?'));
-                }
+        public static function get_instance($file = ''){
+            if(null !== self::$instance){
+                return self::$instance;
             }
+            if('' === $file){
+                wp_die(__('File doesn&#8217;t exist?'));
+            }
+            if(!is_file($file)){
+                wp_die(sprintf(__('File &#8220;%s&#8221; doesn&#8217;t exist?'), $file));
+            }
+            self::$instance = new self($file);
             return self::$instance;
     	}
 
@@ -44,14 +47,23 @@ if(!class_exists('BC_BB_Google_Sans')){
 
     	private function __construct($file = ''){
             $this->file = $file;
-            add_filter('fl_builder_get_google_json', [$this, 'add_google_sans']);
-            add_filter('fl_theme_get_google_json', [$this, 'add_google_sans']);
+            add_action('after_setup_theme', [$this, 'after_setup_theme']);
+            add_action('plugins_loaded', [$this, 'plugins_loaded']);
         }
 
     	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     	//
     	// public
     	//
+    	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+        public function after_setup_theme(){
+            if(!defined('FL_THEME_VERSION')){
+                return;
+            }
+            add_filter('fl_theme_get_google_json', [$this, 'add_google_sans']);
+        }
+
     	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
         public function add_google_sans($json){
@@ -62,6 +74,18 @@ if(!class_exists('BC_BB_Google_Sans')){
                 ],
             ];
             return $json;
+        }
+
+    	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+        public function plugins_loaded(){
+            if(!defined('BC_FUNCTIONS')){
+        		return;
+        	}
+            if(defined('FL_BUILDER_VERSION')){
+                add_filter('fl_builder_get_google_json', [$this, 'add_google_sans']);
+        	}
+            bc_build_update_checker('https://github.com/beavercoffee/bc-bb-bootstrap-4', $this->file, 'bc-bb-bootstrap-4');
         }
 
     	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
